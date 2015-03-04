cf8a818a5e1dee731e6747cd40d6f2698c558cf8
AlanPoon <alan@elixirtech.com>
2015-03-04 15:03:40 +0800

    before removing the dataGroup

--- federalBudget.js
+++ federalBudget.js
@@ -97,7 +97,7 @@ function federalBudgetChart(){
 		window.diagonal = d3.svg.diagonal()
             .projection(function(d) { return [d.y, d.x]; });  
 			
-		  window.vis = d3.select("#body").append("svg:svg")
+		  window.vis = d3.select("#body_"+viewId).append("svg:svg")
             .attr("width", w + m[1] + m[3])
             .attr("height", h + m[0] + m[2])
             .append("svg:g")
